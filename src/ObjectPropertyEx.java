c89ea4ce5247db86775b636efcf5cada3ce6fbae
youminan200 <79958383+youminan200@users.noreply.github.com>
2026-04-07 15:15:02 +0900

    Add ObjectPropertyEx.java from javaworkspace

---
+++ src/ObjectPropertyEx.java
@@ -0,0 +1,16 @@
+class PointHash{
+    private int x, y;
+    public PointHash(int x, int y){
+        this.x = x; this.y = y;
+    }
+}
+
+public class ObjectPropertyEx{
+
+    public static void main(String[] args) {
+        PointHash p = new PointHash(2,3);
+        System.out.println(p.getClass().getName());
+        System.out.println(p.hashCode());
+        System.out.println(p.toString());
+    }
+}
