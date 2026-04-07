597268d4dd8b7c915de5d9000236be59a4e327a1
youminan200 <79958383+youminan200@users.noreply.github.com>
2026-04-07 15:26:30 +0900

    Update ExString.java in shape package

--- shape/ExString.java
+++ shape/ExString.java
@@ -1,5 +1,14 @@
 package shape;
 
-public class java {
+public class ExString {
+    int radius;
+    public String name;
 
+    public ExString(int radius){
+        this.radius=radius;
+    }
+
+    public double getArea(){
+        return 3.14*radius*radius;
+    }
 }
