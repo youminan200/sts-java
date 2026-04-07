a0648243a68ce6f30403151356e2b9cc0ebcb042
youminan200 <79958383+youminan200@users.noreply.github.com>
2026-04-07 14:44:00 +0900

    Add Shape package with Circle and ToStringEx classes

---
+++ src/Shape/ToStringEx.java
@@ -0,0 +1,21 @@
+package Shape;
+class Point{
+    private int x, y;
+    public Point(int x, int y){
+        this.x = x; this.y = y;
+    }
+    @Override
+    public String toString(){
+        return "Point(" + x + "," + y + ")";
+    }
+}
+
+
+public class ToStringEx{
+    public static void main(String[] args){
+        Point a = new Point(2,3);
+        System.out.println(a.toString());
+        System.out.println(a);
+    }
+    
+}
