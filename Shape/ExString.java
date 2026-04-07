45f53519d4db8ee20b876bca0ae33ce58382024d
youminan200 <79958383+youminan200@users.noreply.github.com>
2026-04-07 15:17:08 +0900

    Add ExString.java to Shape package

---
+++ Shape/ExString.java
@@ -0,0 +1,14 @@
+package Shape;
+
+public class ExString {
+    int radius;
+    public String name;
+
+    public ExString(int radius){
+        this.radius=radius;
+    }
+
+    public double getArea(){
+        return 3.14*radius*radius;
+    }
+}
